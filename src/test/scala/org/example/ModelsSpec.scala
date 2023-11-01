76ef6d84055a3048cc9ac1b3a13fa158bf95dbe1
Marek Rehora <m.rehora@gmail.com>
2023-11-01 08:53:10 +0100

    Sonarcloud

---
+++ src/test/scala/org/example/ModelsSpec.scala
@@ -0,0 +1,19 @@
+package org.example
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.example.Models._
+import org.scalatest.matchers.should.Matchers
+
+class ModelsTest extends AnyFunSuite with Matchers {
+  test("Frequency should throw for invalid string") {
+    assertThrows[IllegalArgumentException] {
+      Frequency.stringToFrequency("Invalid")
+    }
+  }
+
+  test("TaskDefinition case class should work") {
+    val instance = Instance("id1", "display1")
+    val task = TaskDefinition(1, "name", Some("details"), "owner", "2021-01-01", Frequency.Monthly, instance)
+    task.name shouldEqual "name"
+  }
+}
